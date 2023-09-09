0c63115ffbdaa330438fdf53cb6c096f6380d2e3
shishirbychapur <shishirbychapur@gmail.com>
2023-09-10 01:28:25 +0800

    Div Change

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -67,32 +67,34 @@ export default function Job(props) {
 
   return (
     <div className={containerClass} onClick={selectJob}>
-      <div className="flex xs:flex-col sm:flex-row justify-between text-left">
-        <img src={props.data.logo} alt="logo" className="w-auto sm:h-10 md:h-12 lg:h-14 h-10 mt-2 mr-2" />
-        <div className="w-11/12 lg:w-9/12">
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-row text-sm">
-              <p className="mr-2 max-w-40">{props.data.company}</p>
-              <p>{props.data.rating} ★</p>
+      <div className="flex flex-row justify-between text-left">
+        <div className="flex flex-row justify-start">
+          <img src={props.data.logo} alt="logo" className="sm:w-10 md:w-12 lg:w-14 sm:h-10 md:h-12 lg:h-14 h-10 mt-2 mr-2" />
+          <div className="w-11/12 lg:w-9/12">
+            <div className="flex xs:flex-col flex-row justify-between">
+              <div className="flex flex-row text-sm">
+                <p className="mr-2 max-w-40">{props.data.company}</p>
+                <p>{props.data.rating} ★</p>
+              </div>
             </div>
-          </div>
-          <p className="max-w-lg font-open-sans font-bold mt-1 line-clamp-2">{props.data.name}</p>
-          <div className="flex flex-row">
-            <p className="font-semibold text-dark-green mr-1">{props.data.date}</p>
+            <p className="max-w-lg font-open-sans font-bold mt-1 line-clamp-2">{props.data.name}</p>
             <div className="flex flex-row">
-              <img className="w-auto h-4 m-0.5" src={location} alt="location" />
-              <p className="lg:ml-1 xs:ml-0.5 text-dark-gray text-sm">{props.data.location}</p>
+              <p className="font-semibold text-dark-green mr-2">{props.data.date}</p>
+              <img className="w-auto h-4 mt-0.5 ml-0.5" src={location} alt="location" />
+              <p className="ml-0.5 lg:ml-1.5 xs:ml-1 text-dark-gray text-sm">{props.data.location}</p>
             </div>
-          </div>
-          <div className="flex flex-row mt-1">
-            <p className="text-sm font-medium">{props.data.pay}</p>
-            { props.data.pay === undefined 
-                ? '' 
-                : <div className="bg-light-gray ml-2 px-2 text-sm rounded-md lg:max-h-10 max-h-6 text-center"><p>EST</p></div>
-            }
+            <div className="max-w-lg mt-1">
+              <p className="text-sm font-medium">
+                {props.data.pay}
+                { props.data.pay === undefined 
+                  ? '' 
+                  : <span className="bg-light-gray ml-2 p-1 text-sm rounded-md lg:max-h-10 max-h-6 text-center">EST</span>
+                }
+              </p>
+            </div>
           </div>
         </div>
-        <div className="bg-light-green h-1/2 rounded-md p-1.5 text-center">
+        <div className="max-w-2/12 bg-light-green h-1/2 rounded-md p-1.5 text-center">
           <p className="text-dark-green font-semibold text-sm">{props.data.tag}</p>
         </div>
       </div>
--- src/components/Selected.jsx
+++ src/components/Selected.jsx
@@ -6,7 +6,7 @@ export default function Selected() {
   const { selected } = React.useContext(SelectedContext);
 
   return (
-    <div className="hidden lg:inline lg:border lg:border-solid lg:border-gray-700 lg:w-2/4 lg:h-screen lg:my-2 lg:p-2 lg:bg-white">
+    <div className="hidden lg:inline lg:border lg:border-solid lg:border-gray-700 lg:w-2/4 lg:h-2/3 lg:my-2 lg:p-2 lg:bg-white">
       <p className="hidden lg:inline lg:font-semibold lg:text-xl">{selected.name}</p>
     </div>
   );
